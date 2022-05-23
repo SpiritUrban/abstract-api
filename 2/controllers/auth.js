f3cf938dd0a230b9618e283b8cd886023096b576
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-05-23 11:08:07 +0300

    refactor

--- 2/controllers/auth.js
+++ 2/controllers/auth.js
@@ -13,30 +13,21 @@ class ChangePasswordController extends Controller {
     conName = 'Change password';
     successMsg = 'Password changed';
     unSuccessMsg = 'Cannot change password!';
-    errMsg = `Error! ${this.unSuccessMsg}`;
-    constructor() { super() }
     do = async _ => this.result = await AuthService.changePassword(this.req.body, this.req.user._id, this.req.user.password);
-    fork = _ => this.result.ok ? this.successDTO : this.unSuccessDTO
 }
 
 class RestoreAccessController extends Controller {
     conName = 'Restore access';
     successMsg = 'Your password has been sent to your mail';
     unSuccessMsg = 'Cannot restore access!';
-    errMsg = `Error! ${this.unSuccessMsg}`;
-    constructor() { super() }
     do = async _ => this.result = await AuthService.restoreAccess(req.body);
-    fork = _ => this.result.ok ? this.successDTO : this.unSuccessDTO
 }
 
 class RestorePasswordController extends Controller {
     conName = 'Restore password';
     successMsg = 'Mail sent!';
     unSuccessMsg = 'Cannot restore password!';
-    errMsg = `Error! ${this.unSuccessMsg}`;
-    constructor() { super() }
     do = async _ => this.result = await AuthService.restorePasswordSimple(req.body);
-    fork = _ => this.result.ok ? this.successDTO : this.unSuccessDTO
 }
 
 export { RegisterController, ChangePasswordController, RestoreAccessController, RestorePasswordController };
